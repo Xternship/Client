c4107adad3e4dd70fd9c41a2dabbc0c3c2fdf14c
Malithi Abayadeera <MalithiA@99x.io>
2024-07-19 18:09:56 +0530

    login

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'color-primary': '#6366F1', 
+        'color-tertiary': '#F3F4F6', 
+        'color-accent': '#3B82F6', 
+        'color-accent-dark': '#1E40AF', 
+      },
+    },
+  },
+  plugins: [],
+};
